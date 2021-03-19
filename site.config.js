12e683951b3ec8bb764c25b463b6ef651dc825f3
dora-admin001 <67258691+dora-admin001@users.noreply.github.com>
2021-03-19 14:31:45 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'e59712827c1a470da4e8b669b08c5b65',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'TCM Database Blog',
+  domain: 'blog.tcm-database.tk',
+  author: 'TCM Database',
 
   // open graph metadata (optional)
   description: 'Example site description',
